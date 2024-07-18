7ec1c0e0d3518b061092618abd09072b7d79747e
altsermaa <altsermaa@gmail.com>
2024-07-18 18:56:24 +0800

    Jul 18 hw

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -9,15 +9,34 @@ function ContactPage() {
     <div>
         <Navbar />
 
-        <div className='my-24 mx-auto lg:w-[1100px]'>
-            <h1 className='text-4xl text-[#181A2A]'>Contact Us</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
-            <div>
-                <div className=''>
-
-                </div>
+        <div className='mt-24 mx-auto lg:w-[895px] px-20'>
+            <div className='pt-3.5 pb-4 px-auto'>
+              <h1 className='text-4xl text-[#181A2A] mb-4'>Contact Us</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
             </div>
-            
+            <div className='flex gap-12 pb-10'>
+                <div className='flex-1 border border-[#f6f6f6] p-4'>
+                  <h1 className='font-bold'>Address</h1>
+                  <p>1328 Oak Ridge Drive, Saint Louis, Missouri</p>
+                </div>
+                <div className='flex-1 border border-[#f6f6f6] p-4'>
+                  <h1 className='font-bold'>Contact</h1>
+                  <p>313-332-8662 <br></br>info@email.com</p>
+                </div>
+
+            </div>
+            <div className='bg-[#f6f6f6] pt-7 pl-9 pr-32 pb-7 mx-auto mb-16'>
+              <b>Leave a Message</b>
+              <div className='flex gap-7 my-5'>
+                <input type='text' placeholder='Your name' className="p-2 border border-gray-300 rounded-md"></input>
+                <input type='text' placeholder='Your email' className="p-2 border border-gray-300 rounded-md"></input>
+              </div>
+              <div className='flex flex-col my-5 gap-5'>
+                <input type='text' placeholder='Subject' className="p-2 border border-gray-300 rounded-md"></input>
+                <input type='text' placeholder='Write a message' className="pb-24 pt-2 pl-2 border border-gray-300 rounded-md"></input>
+              </div>
+              <button className='bg-[#4c6bff] w-fit py-2.5 px-4 text-white rounded-md'>Send Message</button>
+            </div>
         </div>
 
         <Footer />
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className='w-full bg-[#f6f6f6] pt-16 pb-6'>
         <div className='lg:w-[1100px] m-auto bg-[#f6f6f6]'>
-            <div className='flex lg:flex-row flex-col gap-5 text-[#7d7c7f]'>
+            <div className='flex lg:flex-row flex-col gap-5 text-[#7d7c7f] mb-6'>
                 <div className='flex-1 flex flex-col'>
                     <b>About</b>
                     <span className='mb-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</span>
@@ -22,12 +22,16 @@ function Footer() {
                     <FaFacebook />
                     <FaFacebook />
                     <FaFacebook />
-
                 </div>
 
             </div>
-            <div>
-
+            <div className='flex lg:flex-row flex-col gap-16 text-[#7d7c7f] items-center justify-between py-5 border-t-red-500'>
+                <img src='/Copyright Info.png' />
+                <div className='flex gap-8 justify-items-end'>
+                    <p>Terms of Use</p>
+                    <p>Privacy Policy</p>
+                    <p>Cookie Policy</p>
+                </div>
             </div>
         
         </div>
